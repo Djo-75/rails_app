c8f2375cf625925109cdce72bea9bb5220c5040d
Djo-75 <j.pecquet@live.fr>
2023-05-31 16:55:01 +0200

    login feature ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  post "/sessions/user", to: "sessions#create"
   resources :items
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
-  
 end
