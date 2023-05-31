06dac55963a4b03128c4d846bdfec46355af526d
Djo-75 <j.pecquet@live.fr>
2023-05-31 14:14:58 +0200

    relationships models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    has_many :carts
+    has_many :users, through: :carts
+    has_many :orders
+    has_many :users, through: :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+has_many :carts
+has_many :items, through: :carts
+has_many :orders
+has_many :items, through: :orders
+
 end
