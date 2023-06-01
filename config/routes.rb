014735a78e3f7e5bcda9a24633acba4323878d74
Djo-75 <j.pecquet@live.fr>
2023-06-01 19:35:22 +0200

    avatar implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   post "/sessions/user", to: "sessions#create"
   resources :items
-  resources :users
+  resources :users do
+    resources :avatars, only: [:create]
+    end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
